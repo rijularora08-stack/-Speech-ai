11d20183d0cebbdcd00151bd4ac0789f46e78eb3
rijularora08-stack <rijularora08@gmail.com>
2026-07-13 10:28:04 +0530

    Update threat.py

--- threat.py
+++ threat.py
@@ -1,1 +1,31 @@
+def detect_threats(text):
 
+    if not text:
+        return []
+
+    keywords = [
+        "kill",
+        "attack",
+        "bomb",
+        "weapon",
+        "harm",
+        "suicide",
+        "threat",
+        "danger"
+    ]
+
+    results = []
+
+    text_lower = text.lower()
+
+    for word in keywords:
+
+        if word in text_lower:
+            results.append(
+                {
+                    "keyword": word,
+                    "status": "Detected"
+                }
+            )
+
+    return results
